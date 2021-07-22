57f97a149105555c6c1141211b4e70f1f2d346ca
Your Name <justindo720@gmail.com>
2021-07-22 07:21:29 -0400

    Quick Add Todo in home page

--- todo_front/src/views/Home.vue
+++ todo_front/src/views/Home.vue
@@ -18,13 +18,15 @@
     <div class="tile is-ancestor">
       <div class="tile is-vertical is-8">
         <div class="columns mt-5">
-          <div class="tile is-parent column is-offset-1 is-three-fifths" v-if="loggedIn">
+          <div class="tile is-parent column is-offset-1 is-three-fifths quickAdd" v-if="loggedIn">
           <article class="tile is-child notification is-success">
-          <div class="content quickAdd">
+          <div class="content">
             <p class="title">Quick Add Todo</p>
-            <input class="input is-info is-medium is-rounded" type="text" placeholder="Add Your Task Here">
+            <input class="input is-info is-medium is-rounded"
+                   type="text" placeholder="Add Your Task Here"
+                   v-model="newTask" @keyup.enter="postTodo()">
             <div class="buttons is-centered">
-              <button class="button is-info is-outlined mt-5 is-rounded">Add Todo</button>
+              <button class="button is-info mt-5 is-rounded" @click="postTodo()">Add Todo</button>
             </div>
 
           <div class="content">
@@ -56,7 +58,7 @@
             </p>
 
           <div class="content">
-            <div class="box" v-for="(todo, index) in tasks" :key="index">
+            <div class="box " v-for="(todo, index) in tasks" :key="index">
               {{ todo.task }}
             </div>
           </div>
@@ -75,6 +77,7 @@
 // @ is an alias to /src
 import { mapGetters, mapState} from 'vuex'
 import store from "../store";
+import axios from "axios";
 
 export default {
   name: "Home",
@@ -85,6 +88,9 @@ export default {
   data(){
     return{
       showTodosAPI : 'http://127.0.0.1:8000/todos/',
+      newTask: '',
+      root_api_url: 'http://127.0.0.1:8000/all_todos/',
+      visible_todos: [], // Limited to 5 tasks on the home page
     }
   },
   created(){
@@ -95,15 +101,32 @@ export default {
         showTodosAPI: this.showTodosAPI,
         user_id: this.user_id,
         accessToken: this.accessToken
+      }).then(()=>{
+         this.updateVisibleTodos()
       })
     })
 
-  }
+
+  },
+  methods:{
+    postTodo(){
+       axios.post(this.root_api_url,
+          {task:this.newTask, task_owner:this.user_id},
+         {headers:{Authorization: `Bearer ${this.accessToken}`}}).then((response)=>{
+           // If we unshift this will take care of the front page. the task will be added to both home page and view todos
+           this.tasks.unshift(response.data)
+           this.newTask = null
+      })
+    },
+    updateVisibleTodos(){
+      console.log(this.tasks)
+    }
+  },
 };
 </script>
 
 <style scoped>
 .quickAdd{
-  max-height:500px;
+  max-height:250px;
 }
 </style>
